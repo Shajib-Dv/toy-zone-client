76aeb7fe8d70617cd3e6a47ef27cfb87b51a001d
Shajib-Dv <mdsojeeb242@gmail.com>
2023-05-19 00:01:00 +0600

    details btn added

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ToyCard = ({ toy }) => {
   const {
@@ -16,14 +17,22 @@ const ToyCard = ({ toy }) => {
   } = toy;
   return (
     <>
-      <div className="card card-compact glass w-full bg-[#cc06d63e] hover:bg-[#cc06d651] motion">
-        <figure className="h-2/5">
+      <div className="card card-normal h-96 w-full bg-[#cc06d617] hover:bg-[#cc06d626] motion">
+        <figure className="h-80">
           <img src={toyImage} alt="Toy" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{toyName}</h2>
-          <div>
+          <h2 className="card-title text-3xl font-bold text-black">
+            {toyName}
+          </h2>
+          <div className="flex justify-between items-center text-xl font-semibold">
             <p>Price : {price}</p>
+            <p>Rating : {rating}</p>
+          </div>
+          <div>
+            <Link to="/">
+              <button className="primary-btn mt-4">view details</button>
+            </Link>
           </div>
         </div>
       </div>
